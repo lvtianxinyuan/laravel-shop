521a8b9f720c3d5f14f3405b7b4b600f25563147
chongzi <chongzi@gmail.com>
2019-04-29 20:37:40 +0800

    新增收货地址

--- app/Http/Controllers/UserAddressesController.php
+++ app/Http/Controllers/UserAddressesController.php
@@ -2,15 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UserAddressRequest;
+use App\Models\UserAddress;
 use Illuminate\Http\Request;
 
 class UserAddressesController extends Controller
 {
 
+    //收货地址列表
     public function index(Request $request)
     {
         return view('user_addresses.index', [
             'addresses' => $request->user()->addresses,
         ]);
+    }
+
+    //新增收货地址页面
+    public function create()
+    {
+        return view('user_addresses.create_and_edit', ['address' => new UserAddress()]);
+    }
+
+    public function store(UserAddressRequest $request)
+    {
+        $request->user()->addresses()->create($request->only([
+            'province',
+            'city',
+            'district',
+            'address',
+            'zip',
+            'contact_name',
+            'contact_phone',
+        ]));
+
+        return redirect()->route('user_addresses.index');
     }
 }
