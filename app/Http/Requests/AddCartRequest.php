423b1c44403ef8d1b20cbd2b2abddbe5db7371d9
chongzi <chongzi@gmail.com>
2019-05-01 13:58:14 +0800

    评价商品

--- app/Http/Requests/AddCartRequest.php
+++ app/Http/Requests/AddCartRequest.php
@@ -6,15 +6,6 @@ use App\Models\ProductSku;
 
 class AddCartRequest extends Request
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return true;
-    }
 
     public function rules()
     {
